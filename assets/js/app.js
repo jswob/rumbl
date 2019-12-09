2b660eb393cc136bd3c5a99e4a6f5edd1ef4475e
jacek swoboda <jacswob@gmail.com>
2019-12-09 14:21:30 +0100

    adding javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import css from "../css/app.css"
+import css from "../css/app.css";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -9,7 +9,16 @@ import css from "../css/app.css"
 //
 // Import dependencies
 //
-import "phoenix_html"
+import "phoenix_html";
+
+import Player from "./player";
+let video = document.getElementById("video");
+
+if (video) {
+  Player.init(video.id, video.getAttribute("data-player-id"), () => {
+    console.log("player ready!");
+  });
+}
 
 // Import local files
 //
