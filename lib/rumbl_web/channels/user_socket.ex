457d8ff92c68149c6c05006ad8cfe21ac053f3a1
jacek swoboda <jacswob@gmail.com>
2019-12-12 16:32:29 +0100

    socket authentication

--- lib/rumbl_web/channels/user_socket.ex
+++ lib/rumbl_web/channels/user_socket.ex
@@ -4,9 +4,24 @@ defmodule RumblWeb.UserSocket do
   ## Channels
   channel "videos:*", RumblWeb.VideoChannel
 
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  @max_age 2 * 7 * 24 * 60 * 60
+
+  def connect(%{"token" => token}, socket, _connect_info) do
+    case Phoenix.Token.verify(
+           socket,
+           "user socket",
+           token,
+           max_age: @max_age
+         ) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+
+      {:error, _reason} ->
+        :error
+    end
   end
 
-  def id(_socket), do: nil
+  def connect(_params, _socket, _connect_info), do: :error
+
+  def id(socket), do: "user_socket:#{socket.assigns.user_id}"
 end
--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -9,15 +9,20 @@ defmodule RumblWeb.Auth do
     user_id = get_session(conn, :user_id)
 
     cond do
-      conn.assigns[:current_user] -> conn
-      user = user_id && Rumbl.Accounts.get_user(user_id) -> assign(conn, :current_user, user)
-      true -> assign(conn, :current_user, nil)
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user)
+
+      user = user_id && Rumbl.Accounts.get_user(user_id) ->
+        put_current_user(conn, user)
+
+      true ->
+        assign(conn, :current_user, nil)
     end
   end
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
@@ -35,5 +40,13 @@ defmodule RumblWeb.Auth do
       |> redirect(to: Routes.page_path(conn, :index))
       |> halt()
     end
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 end
